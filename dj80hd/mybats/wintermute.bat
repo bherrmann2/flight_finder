8f7ab16ef07b547607dae878e4f97dfe5eea0c6c
werwath <james.werwath@nokia.com>
2013-04-17 16:49:16 -0500

    2 old scripts

---
+++ dj80hd/mybats/wintermute.bat
@@ -0,0 +1,2 @@
+cd c:\wintermute\gui
+..\scripts\activate
---
+++ dj80hd/mybats/zebcopy.bat
@@ -0,0 +1,3 @@
+start ruby c:\scripts\replacedir.rb %1:\_ZEB %2:\_ZEB
+start ruby c:\scripts\replacedir.rb %1:\dj80hd_loves %2:\dj80hd_loves
+start ruby c:\scripts\replacedir.rb %1:\dj80hd %2:\dj80hd
