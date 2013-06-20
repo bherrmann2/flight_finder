78a2d6c348d28f37622983cf321959819f8d3b41
unknown <werwath@gmail.com>
2013-06-20 09:09:00 -0500

    add copytokr8, change source to c drive

---
+++ dj80hd/mybats/copyfromkr8.bat
@@ -0,0 +1,1 @@
+ruby c:\typhon\dj80hd\scripts\replacedir.rb %1:\kr8 c:\kr8 
--- dj80hd/mybats/copytokr8.bat
+++ dj80hd/mybats/copytokr8.bat
@@ -1,1 +1,1 @@
-ruby c:\typhon\dj80hd\scripts\replacedir.rb z:\dj80hd_loves\kr8 %1:\kr8
+ruby c:\typhon\dj80hd\scripts\replacedir.rb c:\kr8 %1:\kr8
