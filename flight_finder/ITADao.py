74e940e14b6d5dd195730c8b757a62807ad170be
HGF <bradherrmann1@gmail.com>
2014-09-28 16:27:10 -0500

    defined initial rest api

--- flight_finder/ITADao.py
+++ flight_finder/ITADao.py
@@ -114,7 +114,6 @@ class CalendarRequestThread (threading.Thread):
         payload = "name=calendar&summarizers=currencyNotice%2CovernightFlightsCalendar%2CitineraryStopCountList%2CitineraryCarrierList%2Ccalendar&format=JSON&inputs=%7B%22slices%22%3A%5B%7B%22origins%22%3A%5B%22"+self.origin+"%22%5D%2C%22originPreferCity%22%3Afalse%2C%22destinations%22%3A%5B%22"+self.dest+"%22%5D%2C%22destinationPreferCity%22%3Afalse%7D%2C%7B%22destinations%22%3A%5B%22"+self.origin+"%22%5D%2C%22destinationPreferCity%22%3Afalse%2C%22origins%22%3A%5B%22"+self.dest+"%22%5D%2C%22originPreferCity%22%3Afalse%7D%5D%2C%22startDate%22%3A%22"+self.start_date+"%22%2C%22layover%22%3A%7B%22max%22%3A"+self.max_length+"%2C%22min%22%3A"+self.min_length+"%7D%2C%22pax%22%3A%7B%22adults%22%3A1%7D%2C%22cabin%22%3A%22COACH%22%2C%22changeOfAirport%22%3Afalse%2C%22checkAvailability%22%3Atrue%2C%22firstDayOfWeek%22%3A%22SUNDAY%22%2C%22endDate%22%3A%22"+self.end_date+"%22%7D"
         page = requests.post(ITADao.url, data=payload, headers=ITADao.headers)
         text = page.text[4:]
-        print text
         data = json.loads(text)
         return ITACalendar(data)
 
@@ -150,7 +149,6 @@ class TripRequestThread (threading.Thread):
         payload = 'name=calendarFollowup&session=' + session + '&summarizers=carrierStopMatrix%2CcurrencyNotice%2CsolutionList%2CitineraryPriceSlider%2CitineraryCarrierList%2CitineraryDepartureTimeRanges%2CitineraryArrivalTimeRanges%2CdurationSliderItinerary%2CitineraryOrigins%2CitineraryDestinations%2CitineraryStopCountList%2CwarningsItinerary&format=JSON&inputs=%7B%22slices%22%3A%5B%7B%22origins%22%3A%5B%22'+self.origin+'%22%5D%2C%22originPreferCity%22%3Afalse%2C%22destinations%22%3A%5B%22' + self.dest + '%22%5D%2C%22destinationPreferCity%22%3Afalse%2C%22date%22%3A%22' + leave + '%22%7D%2C%7B%22destinations%22%3A%5B%22'+self.origin+'%22%5D%2C%22destinationPreferCity%22%3Afalse%2C%22origins%22%3A%5B%22' + self.dest + '%22%5D%2C%22originPreferCity%22%3Afalse%2C%22date%22%3A%22' + ret + '%22%7D%5D%2C%22startDate%22%3A%22' + self.start_date + '%22%2C%22layover%22%3A%7B%22max%22%3A'+self.max_length+'%2C%22min%22%3A'+self.min_length+'%7D%2C%22pax%22%3A%7B%22adults%22%3A1%7D%2C%22cabin%22%3A%22COACH%22%2C%22changeOfAirport%22%3Afalse%2C%22checkAvailability%22%3Atrue%2C%22firstDayOfWeek%22%3A%22SUNDAY%22%2C%22endDate%22%3A%22' + self.end_date + '%22%2C%22page%22%3A%7B%22size%22%3A30%7D%2C%22sorts%22%3A%22pricePerMile%22%7D'
         page = requests.post(ITADao.url, data=payload, headers=ITADao.headers)
         text = page.text[4:]
-        print text
         data = json.loads(text)
         try:
             flights = data['result']['solutionList']['solutions']
--- flight_finder/Rest.py
+++ flight_finder/Rest.py
@@ -1,20 +1,64 @@
-__author__ = 'brherrma'
+__author__ = 'Brad'
 
 from flask import Flask
 import datetime
 from ITADao import ITADao
 
+"""
+API:
+best value
+lowest price
+lowest priced best value
+best valued lowest price
+below price
+below value
+"""
+
 app = Flask(__name__)
 
-@app.route('/best_value/<dest>/<start_date>/<end_date>/<min_length>/<max_length>', methods= ['GET'])
-def get_best_value(dest, start_date, end_date, min_length, max_length):
-    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
-    end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
-    dao = ITADao(dest, start_date, end_date, min_length, max_length)
-    trips = dao.get_trip_data()
+@app.route('/best_value/<origin>/<dest>/<start_date>/<end_date>/<min_length>/<max_length>', methods= ['GET'])
+def get_best_value(origin, dest, start_date, end_date, min_length, max_length):
+    trips = get_trips(origin, dest, start_date, end_date, min_length, max_length)
     trips = trips.find_best_value()
     tlist = trips.get_trips()
 
+@app.route('/lowest_price/<origin>/<dest>/<start_date>/<end_date>/<min_length>/<max_length>', methods= ['GET'])
+def get_lowest_price(origin, dest, start_date, end_date, min_length, max_length):
+    trips = get_trips(origin, dest, start_date, end_date, min_length, max_length)
+    trips = trips.find_cheapest_trips()
+    tlist = trips.get_trips()
+
+@app.route('/below_price/<price>/<origin>/<dest>/<start_date>/<end_date>/<min_length>/<max_length>', methods= ['GET'])
+def get_lowest_price(price, origin, dest, start_date, end_date, min_length, max_length):
+    trips = get_trips(origin, dest, start_date, end_date, min_length, max_length)
+    trips = trips.find_trips_below_price(price)
+    tlist = trips.get_trips()
+
+@app.route('/below_value/<ppm>/<origin>/<dest>/<start_date>/<end_date>/<min_length>/<max_length>', methods= ['GET'])
+def get_lowest_price(ppm, origin, dest, start_date, end_date, min_length, max_length):
+    trips = get_trips(origin, dest, start_date, end_date, min_length, max_length)
+    trips = trips.find_trips_below_ppm(ppm)
+    tlist = trips.get_trips()
+
+@app.route('/best_value_then_lowest_price/<origin>/<dest>/<start_date>/<end_date>/<min_length>/<max_length>', methods= ['GET'])
+def get_lowest_price(origin, dest, start_date, end_date, min_length, max_length):
+    trips = get_trips(origin, dest, start_date, end_date, min_length, max_length)
+    trips = trips.find_best_value()
+    trips = trips.find_cheapest_trips()
+    tlist = trips.get_trips()
+
+@app.route('/lowest_price_then_best_value/<origin>/<dest>/<start_date>/<end_date>/<min_length>/<max_length>', methods= ['GET'])
+def get_lowest_price(origin, dest, start_date, end_date, min_length, max_length):
+    trips = get_trips(origin, dest, start_date, end_date, min_length, max_length)
+    trips = trips.find_cheapest_trips()
+    trips = trips.find_best_value()
+    tlist = trips.get_trips()
+
+def get_trips(origin, dest, start_date, end_date, min_length, max_length):
+    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+    dao = ITADao(origin, dest, start_date, end_date, min_length, max_length)
+    return dao.get_trip_data()
 
 if __name__ == '__main__':
     app.run(debug = True)
