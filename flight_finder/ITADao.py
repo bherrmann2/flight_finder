6f85751b7a82c1a30dd8c17c0301b187c0f9710d
Nick Ruiz <nicholas.ruiz@gmail.com>
2014-10-10 14:41:29 +0200

    Uncomment cookies line

--- flight_finder/ITADao.py
+++ flight_finder/ITADao.py
@@ -6,6 +6,9 @@ import datetime
 import threading
 from ITACalendar import ITACalendar
 from ITATrips import ITATrips
+from FlightRequest import FlightRequest, FlightRequestCollection
+from collections import OrderedDict
+import urllib
 
 """
 Retrieves the trip information from ITA Software
@@ -15,7 +18,9 @@ class ITADao:
     headers = \
     {
         "Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8",
-        "Cookie" : '__utma=269716137.1963755597.1368591497.1368846168.1369958808.3; __utma=241137183.308417330.1368591501.1403974868.1403991047.140; __utmz=241137183.1402970608.133.6.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); searchFormState=%7B%22version%22%3A%220.7.20120825.1%22%2C%22json%22%3A%22%7B%5C%22mode%5C%22%3A%7B%5C%22date%5C%22%3A%5C%22calendar%5C%22%2C%5C%22flightSelection%5C%22%3A%5C%22slice%5C%22%2C%5C%22flightView%5C%22%3A%5C%22slice%5C%22%2C%5C%22pageFlow%5C%22%3A%5C%22calendar%5C%22%2C%5C%22trip%5C%22%3A%5C%22rt%5C%22%2C%5C%22calendarRange%5C%22%3A%5C%2230day%5C%22%7D%2C%5C%22searchForm%5C%22%3A%7B%5C%22mode%5C%22%3A%5C%22advanced%5C%22%2C%5C%22defaults%5C%22%3A%7B%5C%22multiCityRows%5C%22%3A2%7D%2C%5C%22awards%5C%22%3A%5C%22noawards%5C%22%2C%5C%22options%5C%22%3A%7B%5C%22showRoutingCodes%5C%22%3Atrue%2C%5C%22pax%5C%22%3A%5C%22simple%5C%22%7D%7D%7D%22%7D; pricePerMile=%7B%22showControl%22%3Atrue%2C%22showPrice%22%3Atrue%7D; mode.flight=%7B%22view%22%3A%22trip%22%2C%22selection%22%3A%22trip%22%7D; PREF="ID=ddab770e84ffbd166c060d8402d1da5ced52210c7cc1eebfec1ba7ab84841a404416a0bfbf08cb7facfb49969cb56fbd912fb4fc80412f15b2456abb34a0e50f:TM=1403972269:S=052ph5Y8jci3Qu3t"; __utmc=241137183; __utmb=241137183.11.10.1403991047'
+        "Cookie" : '__utma=269716137.1963755597.1368591497.1368846168.1369958808.3; __utma=241137183.308417330.1368591501.1403974868.1403991047.140; __utmz=241137183.1402970608.133.6.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); searchFormState=%7B%22version%22%3A%220.7.20120825.1%22%2C%22json%22%3A%22%7B%5C%22mode%5C%22%3A%7B%5C%22date%5C%22%3A%5C%22calendar%5C%22%2C%5C%22flightSelection%5C%22%3A%5C%22slice%5C%22%2C%5C%22flightView%5C%22%3A%5C%22slice%5C%22%2C%5C%22pageFlow%5C%22%3A%5C%22calendar%5C%22%2C%5C%22trip%5C%22%3A%5C%22rt%5C%22%2C%5C%22calendarRange%5C%22%3A%5C%2230day%5C%22%7D%2C%5C%22searchForm%5C%22%3A%7B%5C%22mode%5C%22%3A%5C%22advanced%5C%22%2C%5C%22defaults%5C%22%3A%7B%5C%22multiCityRows%5C%22%3A2%7D%2C%5C%22awards%5C%22%3A%5C%22noawards%5C%22%2C%5C%22options%5C%22%3A%7B%5C%22showRoutingCodes%5C%22%3Atrue%2C%5C%22pax%5C%22%3A%5C%22simple%5C%22%7D%7D%7D%22%7D; pricePerMile=%7B%22showControl%22%3Atrue%2C%22showPrice%22%3Atrue%7D; mode.flight=%7B%22view%22%3A%22trip%22%2C%22selection%22%3A%22trip%22%7D; PREF="ID=ddab770e84ffbd166c060d8402d1da5ced52210c7cc1eebfec1ba7ab84841a404416a0bfbf08cb7facfb49969cb56fbd912fb4fc80412f15b2456abb34a0e50f:TM=1403972269:S=052ph5Y8jci3Qu3t"; __utmc=241137183; __utmb=241137183.11.10.1403991047',
+        "Cache-Control" : 'no-cache',
+        "Content-Length" : '0',
     }
 
     host = "http://matrix.itasoftware.com"
@@ -24,22 +29,59 @@ class ITADao:
 
     url = host + path
 
-    def __init__(self, origin, dest, start, end, min_length, max_length):
+    def __init__(self):
+        self.flight_requests = FlightRequestCollection()
+        
+    def add_specific_flight(self, origins, destinations, depart_date, date_minus=0, date_plus=0, route_language=None, command_line=None):
+        self.flight_requests.add_single_flight(origins, destinations, depart_date, date_minus, date_plus, route_language, command_line)
+        self.search_type = 'specific'
+        
+    def set_calendar_query(self, origin, dest, start, end, min_length, max_length):
         self.origin = origin
         self.dest = dest
         self.start_date = start
         self.end_date = end
         self.min_length = min_length
         self.max_length = max_length
+        self.search_type = 'calendar'
+        
+    def get_trip_data(self):
+        if self.search_type == 'calendar':
+            return self.get_calendar_trip_data()
+        elif self.search_type == 'specific':
+            return self.get_specific_trip_data()
+        else:
+            print 'Whoops'
+            return None
+        
+    def get_specific_trip_data(self):
+        
+        threads = []
+        trips = []
+        
+        print self.flight_requests.to_json()
+        t = MultiDestinationRequestThread(self.flight_requests, trips)
+        t.start()
+        threads.append(t)
+        
+        for t in threads:
+            t.join()
+
+        #combine all the returned trip data from all the threads
+        ita_trips = ITATrips()
+        for trip_data in trips:
+            ita_trips._add_trips(trip_data)
+
+        return ita_trips
 
     """
     Gets the trip data from ITA for the destination and date range
     """
-    def get_trip_data(self):
+    def get_calendar_trip_data(self):
         #breaks up the date in range into sections of 30 days each.
         #this is done because ITA doesn't handle chunks larger than 30 days each that well
         amt_of_days = (self.end_date-self.start_date).days
-        searches_needed = amt_of_days/30
+        searches_needed = amt_of_days/30 # TODO: if the search is less than 30 days, this doesn't work.
         months = []
         threads = []
         start = self.start_date
@@ -87,11 +129,52 @@ class ITADao:
             ita_trips._add_trips(trip_data)
 
         return ita_trips
+    
+class MultiDestinationRequestThread(threading.Thread):
+    """
+    Thread that handles the request/response for multi-destination searches from ITA
+    """
+    def __init__(self, flight_requests, results):
+        threading.Thread.__init__(self)
+        self.flight_requests = flight_requests
+        self.results = results
+        
+    def run(self):
+        trips = self.__get_trip_data()
+        self.results.append(trips)
+        
+    def __get_trip_data(self):
+        summarizers = ["carrierStopMatrix", "currencyNotice", "solutionList", "itineraryPriceSlider", "itineraryCarrierList", "itineraryDepartureTimeRanges", "itineraryArrivalTimeRanges", "durationSliderItinerary", "itineraryOrigins", "itineraryDestinations", "itineraryStopCountList", "warningsItinerary"]
+        commands = OrderedDict()
+        commands['name'] = 'specificDates'
+        commands['summarizers'] = '%2C'.join(summarizers)
+        commands['format'] = 'JSON'
+        #commands['inputs'] = json.dumps(self.flight_requests.to_json())
+        commands['inputs'] = urllib.quote_plus('{"slices":[{"origins":["MIL","FCO","PSA","BLQ","VCE"],"originPreferCity":false,"routeLanguage":"X+","destinations":["SEA"],"destinationPreferCity":true,"date":"2014-10-21","isArrivalDate":false,"dateModifier":{"minus":2,"plus":2}},{"destinations":["MIL","FCO","PSA","BLQ","VCE"],"destinationPreferCity":false,"origins":["SEA"],"originPreferCity":true,"date":"2014-11-07","isArrivalDate":false,"dateModifier":{"minus":1,"plus":1}}],"pax":{"adults":3,"children":1,"seniors":1,"infantsInSeat":1,"youth":1,"infantsInLap":1},"cabin":"COACH","maxStopCount":2,"changeOfAirport":true,"checkAvailability":true,"currency":"USD","salesCity":"SEA","page":{"size":30},"sorts":"default"}')
+        
+        print self.flight_requests
+        print self.flight_requests.to_json()
+        print commands['inputs']
+        
+        payload = '&'.join(['{0}={1}'.format(key,value) for key, value in commands.iteritems()])
+        
+        print payload
+            
+        page = requests.post(ITADao.url, data=payload, headers=ITADao.headers)
+        text = page.text[4:]
+        print text
+        data = json.loads(text)
+        try:
+            flights = data['result']['solutionList']['solutions']
+            return flights
+        except KeyError:
+            print "Error. Couldn't get trip info"
+            return []
 
-"""
-Thread that handles the request/response for the calendar data from ITA
-"""
 class CalendarRequestThread (threading.Thread):
+    """
+    Thread that handles the request/response for the calendar data from ITA
+    """
     #needs to be refactored. too many args
     def __init__(self, origin, dest, start_date, end_date, min_length, max_length, results):
         threading.Thread.__init__(self)
@@ -107,11 +190,12 @@ class CalendarRequestThread (threading.Thread):
         calendar = self.__get_calendar_data()
         self.results.append(calendar)
 
-    """
-    gets the calendar data from ITA
-    """
     def __get_calendar_data(self):
+        """
+        gets the calendar data from ITA
+        """
         payload = "name=calendar&summarizers=currencyNotice%2CovernightFlightsCalendar%2CitineraryStopCountList%2CitineraryCarrierList%2Ccalendar&format=JSON&inputs=%7B%22slices%22%3A%5B%7B%22origins%22%3A%5B%22"+self.origin+"%22%5D%2C%22originPreferCity%22%3Afalse%2C%22destinations%22%3A%5B%22"+self.dest+"%22%5D%2C%22destinationPreferCity%22%3Afalse%7D%2C%7B%22destinations%22%3A%5B%22"+self.origin+"%22%5D%2C%22destinationPreferCity%22%3Afalse%2C%22origins%22%3A%5B%22"+self.dest+"%22%5D%2C%22originPreferCity%22%3Afalse%7D%5D%2C%22startDate%22%3A%22"+self.start_date+"%22%2C%22layover%22%3A%7B%22max%22%3A"+self.max_length+"%2C%22min%22%3A"+self.min_length+"%7D%2C%22pax%22%3A%7B%22adults%22%3A1%7D%2C%22cabin%22%3A%22COACH%22%2C%22changeOfAirport%22%3Afalse%2C%22checkAvailability%22%3Atrue%2C%22firstDayOfWeek%22%3A%22SUNDAY%22%2C%22endDate%22%3A%22"+self.end_date+"%22%7D"
+        print 'get_calendar_data', payload
         page = requests.post(ITADao.url, data=payload, headers=ITADao.headers)
         text = page.text[4:]
         print text
@@ -148,6 +232,7 @@ class TripRequestThread (threading.Thread):
     def get_trips(self, leave, ret, session):
         #need to get the trip length from the calendar and put in max and min
         payload = 'name=calendarFollowup&session=' + session + '&summarizers=carrierStopMatrix%2CcurrencyNotice%2CsolutionList%2CitineraryPriceSlider%2CitineraryCarrierList%2CitineraryDepartureTimeRanges%2CitineraryArrivalTimeRanges%2CdurationSliderItinerary%2CitineraryOrigins%2CitineraryDestinations%2CitineraryStopCountList%2CwarningsItinerary&format=JSON&inputs=%7B%22slices%22%3A%5B%7B%22origins%22%3A%5B%22'+self.origin+'%22%5D%2C%22originPreferCity%22%3Afalse%2C%22destinations%22%3A%5B%22' + self.dest + '%22%5D%2C%22destinationPreferCity%22%3Afalse%2C%22date%22%3A%22' + leave + '%22%7D%2C%7B%22destinations%22%3A%5B%22'+self.origin+'%22%5D%2C%22destinationPreferCity%22%3Afalse%2C%22origins%22%3A%5B%22' + self.dest + '%22%5D%2C%22originPreferCity%22%3Afalse%2C%22date%22%3A%22' + ret + '%22%7D%5D%2C%22startDate%22%3A%22' + self.start_date + '%22%2C%22layover%22%3A%7B%22max%22%3A'+self.max_length+'%2C%22min%22%3A'+self.min_length+'%7D%2C%22pax%22%3A%7B%22adults%22%3A1%7D%2C%22cabin%22%3A%22COACH%22%2C%22changeOfAirport%22%3Afalse%2C%22checkAvailability%22%3Atrue%2C%22firstDayOfWeek%22%3A%22SUNDAY%22%2C%22endDate%22%3A%22' + self.end_date + '%22%2C%22page%22%3A%7B%22size%22%3A30%7D%2C%22sorts%22%3A%22pricePerMile%22%7D'
+        print 'get_trips', payload
         page = requests.post(ITADao.url, data=payload, headers=ITADao.headers)
         text = page.text[4:]
         print text
